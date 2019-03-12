5bd693cb18061a6fd2dbc29f5ef144e4a7e916bd
JosephYusufov <jyusufov00@stuy.edu>
2019-03-12 19:17:42 -0400

    First attempt @ List without OOP

--- List_inArraySlots.java
+++ List_inArraySlots.java
@@ -10,7 +10,7 @@ public class List_inArraySlots {
     private double[] doubleElements;
     private String[] stringElements;
     private int filledElements; // the number of elements in this list
-    
+
     /* type identifier for each element
        That is, typeOfElements[i] == 0 means element i is an integer;
                                      1 means element i is a double;
@@ -26,22 +26,35 @@ public class List_inArraySlots {
       Construct an empty list with a small initial capacity.
      */
     public List_inArraySlots() {
+        intElements = int[INITIAL_CAPACITY];
+        doubleElements = double[INITIAL_CAPACITY];
+        stringElements = String[INITIAL_CAPACITY];
+        typeOfElements = int[INITIAL_CAPACITY];
     }
 
 
     /**
       @return the number of elements in this list
      */
-    // public int size() {
-    // }
+    public int size() {
+        return filledElements;
+    }
 
 
      /**
        @return a string representation of this list,
        in [a,b,c,] format
       */
-    // public String toString() {
-    // }
+    public String toString() {
+        String toReturn = "\n";
+        for (int index = 0; index < filledElements ; index ++) {
+            toReturn += "\t"
+            if (typeOfElements[index] == 0) toReturn += intElements[index];
+            if (typeOfElements[index] == 1) toReturn += doubleElements[index];
+            if (typeOfElements[index] == 2) toReturn += stringElements[index];
+            toReturn += "\n"
+        }
+    }
 
 
     /**
@@ -49,19 +62,47 @@ public class List_inArraySlots {
 
       @return true, in keeping with conventions yet to be discussed
      */
-     // public boolean add( int type   // same meaning as in typeOfElements
-                       // , int    intValue
-                       // , double doubleValue
-                       // , String stringValue
-                       // ) {
-     // }
+    public boolean add( int type   // same meaning as in typeOfElements
+                       , int    intValue
+                       , double doubleValue
+                       , String stringValue
+                       ) {
 
+        if (filledElements >= typeOfElements.length) expand(); //expansion if needed
+
+        intElements[filledElements] = intValue;
+        doubleElements[filledElements] = doubleValue;
+        stringElements[filledElements] = stringValue;
+        typeOfElements[filledElements] = type;
+        filledElements++;
+
+        return true; //I wish I knew why
+
+     }
 
     /**
       Double the capacity of the List_inArraySlots,
       preserving existing data.
      */
-     // private void expand() {
+    private void expand() {
+        int[] bigIntElements = new int[intElements.length * 2]
+        int[] bigDoubleElements = new double[doubleElements.length * 2]
+        int[] bigStringElements = new String[stringElements.length * 2]
+        int[] bigTypeOfElements = new int[typeOfElements.length * 2]
+
+        for (int index = 0; index < filledElements; index++) { //copy each element of the old array into the corresponding
+            bigIntElements[index] = intElements[index];
+            bigDoubleElements[index] = doubleElements[index];
+            bigStringElements[index] = stringElements[index];
+            bigTypeOfElements[index] = typeOfElements[index];
+        }
+
+        intElements = bigIntElements;
+        doubleElements = bigDoubleElements;
+        stringElements = bigStringElements;
+        typeOfElements = bigTypeOfElements;
+    }
+
         // System.out.println( "expand... (for debugging)");
            // /* S.O.P. rules for debugging:
               // Working methods should be silent. But during
@@ -69,5 +110,5 @@ public class List_inArraySlots {
               // this method is called when that is appropriate.
               // So test using the println(), then comment it out.
               // */
-     // }
+     }
 }
