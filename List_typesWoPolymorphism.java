1f9c2a9a434877766265f71f7a4722b48948fe5a
Joseph Yusufov <47545002+JosephYusufov@users.noreply.github.com>
2019-03-13 18:58:54 -0400

    unnecessary file

--- List_typesWoPolymorphism.java
+++
@@ -1,5 +0,0 @@
-public class List_typesWoPolymorphism{
-private String[] refArray_String = new String[];
-private int[] refArray_int = new int[];
-private double[] refArray_double = new double[];
-}
