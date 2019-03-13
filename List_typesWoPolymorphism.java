9330a3ddc3363443f3cc033ec7ad57c56b4bf550
Leonard Melnik <leonard@onedr.io>
2019-03-12 22:39:12 -0400

    Basic things

--- List_typesWoPolymorphism.java
+++ List_typesWoPolymorphism.java
@@ -1,3 +1,5 @@
 public class List_typesWoPolymorphism{
-    
+private String[] refArray_String = new String[];
+private int[] refArray_int = new int[];
+private double[] refArray_double = new double[];
 }
--- UserOfList.java
+++ UserOfList.java
@@ -1,3 +1,6 @@
 public class UserOfList{
-    
+  public static void main(String[] args) {
+
+
+  }
 }
